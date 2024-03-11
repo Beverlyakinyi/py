829e41315701f4757aa9006a12005673b16540de
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-03-11 13:49:32 +0300

    added files

---
+++ index.py
@@ -0,0 +1,29 @@
+# Calculate the result of 8 divided by 3 using both regular division and floor division in Python.
+# a=8
+b=3
+c=a/b
+
+
+d=a//b
+# Explain the use of the in operator in Python with a practical example.
+
+txt=(1,2,3,4)
+print(2 in txt)
+# Write a Python expression to find the product of two numbers.
+e=a*b
+print(e)
+# Declare a variable in Python and assign it the value of a floating-point number. Determine its data type using the type() function.
+f=float (40)
+print(4)
+print(type(f))
+# What is the difference between a list and a tuple in Python? Provide an example of each.
+a=("mangoes","apple","grapes")
+print(type(a))
+
+a=("mangoes","grapes","lemon")
+print(type(a))
+# Create a dictionary in Python representing a person with keys for "name," "age," and "city."
+a="beverly"
+b="age"
+c="rwanda"
+d="beverly" + [20] + "rwanda"
